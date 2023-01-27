efe38b3d1af224a084be367fae0ec51508b22cc5
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-01-28 03:56:56 +0700

    tests folder added

---
+++ src/tests/tests_includes.h
@@ -0,0 +1,12 @@
+#ifndef SRC_TESTING_TESTS_INCLUDES_H_
+#define SRC_TESTING_TESTS_INCLUDES_H_
+
+#include <check.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "../s21_string.h"
+
+#endif  // SRC_TESTING_TESTS_INCLUDES_H_
