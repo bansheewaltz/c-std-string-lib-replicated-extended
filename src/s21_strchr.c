e5618c5248711ac0c4448191362df598aad700aa
Koffing Alfred <koffinga@se-c7.nsk.21-school.ru>
2023-01-29 21:15:05 +0700

    test

--- src/s21_strchr.c
+++ src/s21_strchr.c
@@ -4,7 +4,7 @@
 #include "s21_string.h"
 
 char *s21_strchr(const char *str, int c) {
-  int i;
+  int i = 0;
   while ((str[i] != '\0') && (str[i] != c)) i++;
   return c == str[i] ? (char *)str + i : NULL;
 }
