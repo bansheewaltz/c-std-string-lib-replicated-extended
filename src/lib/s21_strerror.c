de7a2f868306b579b9834445dc3c320c753f15f9
Sergey Nepomnyashchikh <nep0m@mail.ru>
2023-03-06 12:18:38 +0700

    strerror leak fix

--- src/lib/s21_strerror.c
+++ src/lib/s21_strerror.c
@@ -4,11 +4,11 @@
 #include "s21_string.h"
 
 char *s21_strerror(int errnum) {
-  char *stringErr = (char *)malloc(200 * sizeof(char));
+  static char stringErr[200] = "";
   char *arrErre[200] = arrErr;
 
   if (errnum < ERROUTSIZE && errnum >= 0) {
-    stringErr = arrErre[errnum];
+    s21_strcpy(stringErr, arrErre[errnum]);
   } else {
     s21_sprintf(stringErr, "%s%d", ERROUT, errnum);
   }
