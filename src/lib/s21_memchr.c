a015f1a1ac4eae4ec5a4b80ec564e533eabd5132
Sergey Nepomnyashchikh <nep0m@mail.ru>
2023-03-07 02:37:59 +0700

    memchr and strpbrk multiply return fixed

--- src/lib/s21_memchr.c
+++ src/lib/s21_memchr.c
@@ -1,11 +1,11 @@
 #include "s21_string.h"
 
 void *s21_memchr(const void *str, int c, s21_size_t n) {
-  s21_size_t i;
-  for (i = 0; i < n; ++i) {
+  void *result = s21_NULL;
+  for (s21_size_t i = 0; i < n; ++i) {
     if (*((char *)str + i) == c) {
-      return (char *)str + i;
+      result = (void *)((char *)str + i);
     }
   }
-  return s21_NULL;
+  return result;
 }
