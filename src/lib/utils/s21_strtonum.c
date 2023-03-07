034b17456a34c346c5e714885e926aaaa3037dac
Sergey Nepomnyashchikh <nep0m@mail.ru>
2023-03-07 23:22:02 +0700

    sscanf nan and inf fix

--- src/lib/utils/s21_strtonum.c
+++ src/lib/utils/s21_strtonum.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 #include "s21_utils.h"
 
 int char_value(char *symbol, int base);  // returns value of char depends on
                                          // base
+long s21_strtol(char *str, char **end_ptr, int base);
+long double s21_strtod(char *str, char **end_ptr);
+int check_nan(char *str);
+int check_inf(char *str);
 
 long s21_strtol(char *str, char **end_ptr, int base) {
   long result = 0;
@@ -56,33 +60,41 @@ long double s21_strtod(char *str, char **end_ptr) {
     sign = -1;
     ptr++;
   }
-  int tmp = char_value(ptr, 10);
-  while (tmp != -1) {
-    result = result * 10 + tmp;
-    ptr++;
-    tmp = char_value(ptr, 10);
-  }
-  if (*ptr == '.') {
-    double base = 0.1;
-    ptr++;
-    tmp = char_value(ptr, 10);
+  if (check_nan(ptr) == 1) {
+    result = +0.0 / +0.0;
+    ptr += 3;
+  } else if (check_inf(ptr) == 1) {
+    result = 1.0 / 0.0;
+    ptr += 3;
+  } else {
+    int tmp = char_value(ptr, 10);
     while (tmp != -1) {
-      result += base * tmp;
-      base /= 10;
+      result = result * 10 + tmp;
       ptr++;
       tmp = char_value(ptr, 10);
     }
-  }
-  if ((*ptr == 'E') || (*ptr == 'e')) {
-    ptr++;
-    int exp = s21_strtol(ptr, &ptr, 10);
-    if (exp < 0) {
-      for (long int i = -1; i >= exp; i--) {
-        result /= 10.;
+    if (*ptr == '.') {
+      double base = 0.1;
+      ptr++;
+      tmp = char_value(ptr, 10);
+      while (tmp != -1) {
+        result += base * tmp;
+        base /= 10;
+        ptr++;
+        tmp = char_value(ptr, 10);
       }
-    } else if (exp > 0) {
-      for (long int i = 1; i <= exp; i++) {
-        result *= 10.;
+    }
+    if ((*ptr == 'E') || (*ptr == 'e')) {
+      ptr++;
+      int exp = s21_strtol(ptr, &ptr, 10);
+      if (exp < 0) {
+        for (long int i = -1; i >= exp; i--) {
+          result /= 10.;
+        }
+      } else if (exp > 0) {
+        for (long int i = 1; i <= exp; i++) {
+          result *= 10.;
+        }
       }
     }
   }
@@ -115,4 +127,34 @@ char *skip_spaces(char *str) {
     ptr++;
   }
   return ptr;
+}
+
+int check_nan(char *str) {
+  int result = 1;
+  char tmp[4] = "";
+  char *nan = "nan";
+  s21_strncpy(tmp, str, 3);
+  tmp[3] = '\0';
+  char *tmp_lower = s21_to_lower(tmp);
+  for (int i = 0; i < 3; i++) {
+    if (tmp_lower[i] != nan[i]) {
+      result = 0;
+    }
+  }
+  return result;
+}
+
+int check_inf(char *str) {
+  int result = 1;
+  char tmp[4] = "";
+  char *inf = "inf";
+  s21_strncpy(tmp, str, 3);
+  tmp[3] = '\0';
+  char *tmp_lower = s21_to_lower(tmp);
+  for (int i = 0; i < 3; i++) {
+    if (tmp_lower[i] != inf[i]) {
+      result = 0;
+    }
+  }
+  return result;
 }
