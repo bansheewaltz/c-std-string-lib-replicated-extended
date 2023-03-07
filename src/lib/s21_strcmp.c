21cd9e77688baed1fcf7e26bd2fd67913f95ae91
Hazzea Pitts <hazzeapi@ob-a1.nsk.21-school.ru>
2023-03-07 18:14:01 +0700

    fix_strcmp

--- src/lib/s21_strcmp.c
+++ src/lib/s21_strcmp.c
@@ -3,13 +3,13 @@
 int s21_strcmp(const char *str1, const char *str2) {
   int result = 0, fend = 0;
   while (fend == 0) {
+    if (*str1 == '\0') {
+      fend = 1;
+    }
     if (*str1 == *str2) {
       str1++;
       str2++;
-      if (*str1 == '\0') {
-        result = 0;
-        fend = 1;
-      }
+
     } else {
       result = ((*str1 < *str2) ? -1 : 1);
       fend = 1;
