6523f3ac46c2472dfa59f731287e92a38f6e8822
Anton Grevtsov <grevtsov.apn@gmail.com>
2023-03-07 18:05:38 +0700

    Makefile miniverter target fixed. Valgrind added to additional tests. strcmp fixed

--- src/lib/s21_strcmp.c
+++ src/lib/s21_strcmp.c
@@ -1,6 +1,10 @@
 #include "s21_string.h"
 
 int s21_strcmp(const char *str1, const char *str2) {
+  if (s21_strlen(str1) == 0 && s21_strlen(str2) == 0) {
+    return 0;
+  }
+
   int result = 0, fend = 0;
   while (fend == 0) {
     if (*str1 == *str2) {
@@ -15,5 +19,6 @@ int s21_strcmp(const char *str1, const char *str2) {
       fend = 1;
     }
   }
+
   return result;
 }
